5ff7414891a52e80f711920493a68e005fee30d3
Tung <nguyen.tung@outlook.de>
2022-10-14 08:44:57 +0200

    Updates

---
+++ precache-manifest.425197a1d915894a19f2ac46dea3e6c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f40067949daa84ae8ec4",
+    "url": "/native/static/css/main.5d6f1630.chunk.css"
+  },
+  {
+    "revision": "f40067949daa84ae8ec4",
+    "url": "/native/static/js/main.f4006794.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/native/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/native/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "a5edeebcc45b59712cfd",
+    "url": "/native/static/js/runtime~main.a5edeebc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/native/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a3a13ad8cd6e8590e374eeb439085d3e",
+    "url": "/native/index.html"
+  }
+];
